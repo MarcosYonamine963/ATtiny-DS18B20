16818273d899aab233632dacc29ee845b95dd3b7
MarcosYonamine963 <marcos.m.yonamine@gmail.com>
2024-06-30 00:07:30 -0300

    v2.0.0
    removed serial communication, added I2C LCD 16x2, changed pins

--- Temperature_Sensor/Temperature_Sensor.ino
+++ Temperature_Sensor/Temperature_Sensor.ino
@@ -3,36 +3,60 @@ Sketch for Arduino IDE
 IDE version: 1.8.19
 */
 
-/* 
+/*   
+Author: Marcos Yonamine
+https://github.com/MarcosYonamine963/ATtiny-DS18B20
+
+v2.0.0
+29/jun/2024
+
 Board: ATtiny85
 Clock: Internal 8MHz
 
 Flash using Arduino as ISP:
 https://gist.github.com/MarcosYonamine963/b8ca19667ada640c18193f8fc0057c47
 
-Author: Marcos Yonamine
-https://github.com/MarcosYonamine963/ATtiny-DS18B20
-
 Dependencies:
 OneWire.h (Paul Stoffregen)
 DallasTemperature.h (Miles Burton)
+TinyWireM.h (Adafruit)
 
-v1.0.0
-05/jan/2024
+https://github.com/platisd/ATtiny85-LCD-library (modified, files at project tree)
+
 */
 
+
+/*
+Changelogs:
+V2.0.0: removed serial communication (to free memory)
+        added I2C LCD 16x2 display
+        
+v1.0.0: initial version
+*/
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <SoftwareSerial.h>
 
-// Serial comunication
-#define RX PB2
-#define TX PB3
+// LCD I2C Display libs
+#include <TinyWireM.h>
+#include <LiquidCrystal_attiny.h> // modified from source. Use mc_print(const char c[], uint8_t col, uint8_t line)
 
-#define ONE_WIRE_BUS PB0
+/*
+I2C interface (LCD Display, PFC8574):
+Must connect 4k7 pullup resistors
+PB0: SDA
+PB2: SCL
 
-SoftwareSerial Serial(RX, TX);
+OneWire Interface
+Must connect 1k pullup resistor
+PB3: OneWire DS18B20
+
+*/
+
+#define ONE_WIRE_BUS PB3
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 // Setup a oneWire instance to communicate with any OneWire devices
@@ -43,20 +67,48 @@ DallasTemperature sensors(&oneWire);
 
 void setup(void)
 {
-  // Start serial communication for debugging purposes
-  Serial.begin(9600);
-  // Start up the library
   sensors.begin();
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.mc_print("Jiga One-Wire", 0, 0);
+  delay(2000);
 }
 
+
+float tempta_val;
+
+float get_tempta_val(void);
+void print_tempta_val(float val);
+
 void loop(void){ 
+  lcd.mc_print("Jiga One-Wire", 0, 0);
+
+  tempta_val = get_tempta_val();
+  print_tempta_val(tempta_val);
+ 
+  delay(10);
+
+}
+
+float get_tempta_val(void)
+{
   // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
   sensors.requestTemperatures(); 
-  
-  Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  Serial.print(sensors.getTempCByIndex(0)); 
-  Serial.print(" - Fahrenheit temperature: ");
-  Serial.println(sensors.getTempFByIndex(0));
-  delay(1000);
+  return sensors.getTempCByIndex(0);
+}
+
+void print_tempta_val(float val)
+{
+  char strBuffer[8];
+  char strTempta[8];
+
+  // sprintf dont work with float on arduino. Use dtostrf to convert float (double) to str.
+  // https://stackoverflow.com/questions/27651012/arduino-sprintf-float-not-formatting
+  // 5 is minimum width, 2 is precision.
+  dtostrf(val, 5, 2, strTempta);
+  sprintf(strBuffer, "%s C", strTempta);
+  lcd.clear();
+  lcd.mc_print(strBuffer, 0, 1);
 }
